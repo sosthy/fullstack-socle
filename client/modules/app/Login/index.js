bd4c7a0578e777d7a561ca2025feef5bbdc1d972
s.nouebissi <s.nouebissi@itgstore-consulting.com>
2020-05-11 19:34:18 +0100

    ajout du formulaire de login

--- client/modules/app/Login/index.js
+++ client/modules/app/Login/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { signin } from "../../session/api";
-import "./styles.css";
+import styles from "./style.module.css";
 import LoginForm from "../LoginForm";
 
 const Login = () => {
@@ -18,22 +18,25 @@ const Login = () => {
     e.preventDefault();
   };
 
-  const handleOnChange = (e, data) => {
-    let { name, value } = data;
-    value = name === "remember" ? data.checked : value;
+  const handleOnChange = (e) => {
+    const target = e.target;
+    const value = target.name === "remember" ? target.checked : target.value;
+    const name = target.name;
     setCredentials({ ...credentials, [name]: value });
   };
 
   return (
-    <div className="login-container">
-      <div></div>
-      <main>
-        <h1>Login</h1>
-        <LoginForm
-          credentials={credentials}
-          handleOnChange={handleOnChange}
-          handleOnSubmit={handleOnSubmit}
-        />
+    <div className={styles.loginContainer}>
+      <div className={styles.leftContent}></div>
+      <main className={styles.mainBlock}>
+        <nav className={styles.mainNav}></nav>
+        <div className={styles.authContent}>
+          <LoginForm
+            credentials={credentials}
+            handleOnChange={handleOnChange}
+            handleOnSubmit={handleOnSubmit}
+          />
+        </div>
       </main>
     </div>
   );
--- client/modules/app/LoginForm/index.js
+++ client/modules/app/LoginForm/index.js
@@ -1,35 +1,58 @@
 import React from "react";
-import { Form, Button } from "semantic-ui-react";
+import styles from "./style.module.css";
 
 const LoginForm = ({ credentials, handleOnChange, handleOnSubmit }) => (
-  <Form onSubmit={handleOnSubmit}>
-    <Form.Field>
-      <Form.Input
-        label="Username"
+  <form className="ui form" onSubmit={handleOnSubmit}>
+    <h1 className="ui header">Salut, Bienvenue</h1>
+    <p className={styles.titleDesc}>
+      Log in to your Thunder account to get back your codes. Or new user Sign
+      up.
+    </p>
+    <div className="field">
+      <label>Username</label>
+      <input
+        type="text"
         name="username"
+        placeholder="Username"
         onChange={handleOnChange}
         value={credentials.username}
       />
-    </Form.Field>
-    <Form.Field>
-      <Form.Input
-        label="Password"
-        name="password"
+    </div>
+    <div className="field">
+      <label>Password</label>
+      <input
         type="password"
+        name="password"
+        placeholder="Password"
         onChange={handleOnChange}
         value={credentials.password}
       />
-    </Form.Field>
-    <Form.Field>
-      <Form.Checkbox
-        label="Remember"
-        name="remember"
-        checked={credentials.remember}
-        onChange={handleOnChange}
-      />
-    </Form.Field>
-    <Button type="submit">Submit</Button>
-  </Form>
+    </div>
+    <div className="field">
+      <div className={styles.buttonConnectMarginTop}>
+        <button className="fluid huge primary ui button" type="submit">
+          Submit
+        </button>
+      </div>
+    </div>
+    <div className={"field"}>
+      <div className={styles.sessionBlock}>
+        <div className="ui checkbox">
+          <input
+            id="remember"
+            name="remember"
+            type="checkbox"
+            tabIndex="0"
+            className="hidden"
+            onChange={handleOnChange}
+            checked={credentials.remember}
+          />
+          <label htmlFor="remember">Remember Me</label>
+        </div>
+        <a href="#">Forgot password ?</a>
+      </div>
+    </div>
+  </form>
 );
 
 export default LoginForm;
